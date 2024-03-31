4099c4b4f56e68747fd4fe6d42aa4879cb4aec15
gil-x <gino.ladowitch@gmail.com>
2024-03-31 21:24:19 +0200

    Inputs keys arrows, test scene, font.

---
+++ src/tests/test_controls.gd
@@ -0,0 +1,59 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func disable_arrow(arrow: Label) -> void:
+	arrow.modulate = Color.WHITE
+
+func disable_arrows() -> void:
+	for arrow in %Panel.get_children():
+		arrow.modulate = Color.WHITE
+		
+func highlight_arrow(arrow: Label) -> void:
+	#Debug.print(arrow.theme_override_colors.font_color)
+	arrow.modulate = Color.YELLOW_GREEN
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Detect key press
+	if Input.is_action_pressed("ui_up"):
+		highlight_arrow(%Up)
+	if Input.is_action_pressed("ui_down"):
+		highlight_arrow(%Down)
+	if Input.is_action_pressed("ui_left"):
+		highlight_arrow(%Left)
+	if Input.is_action_pressed("ui_right"):
+		highlight_arrow(%Right)
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left"):
+		highlight_arrow(%UpLeft)
+		disable_arrow(%Up)
+		disable_arrow(%Left)
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
+		highlight_arrow(%UpRight)
+		disable_arrow(%Up)
+		disable_arrow(%Right)
+	if Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
+		highlight_arrow(%DownLeft)
+		disable_arrow(%Down)
+		disable_arrow(%Left)
+	if Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
+		highlight_arrow(%DownRight)
+		disable_arrow(%Down)
+		disable_arrow(%Right)
+	# Detect release
+	if Input.is_action_just_released("ui_up"):
+		disable_arrows()
+		#disable_arrow(%Up)
+	if Input.is_action_just_released("ui_down"):
+		disable_arrows()
+		#disable_arrow(%Down)
+	if Input.is_action_just_released("ui_left"):
+		disable_arrows()
+		#disable_arrow(%Left)
+	if Input.is_action_just_released("ui_right"):
+		disable_arrows()
+		#disable_arrow(%Right)
+	
